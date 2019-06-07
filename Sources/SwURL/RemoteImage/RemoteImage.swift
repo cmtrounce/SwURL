cb15bad40a8f63f3cc83b24c1abf254f3cd2cb8f
Callum Trounce <trounce@d-tt.nl>
2019-06-07 19:14:40 +0200

    compiler problems.

--- Sources/SwURL/RemoteImage/RemoteImage.swift
+++ Sources/SwURL/RemoteImage/RemoteImage.swift
@@ -30,7 +30,6 @@ class RemoteImage: BindableObject {
         request = ImageLoader.shared.load(url: url).map { cgImage -> Image in
             Image.init(cgImage, scale: 1, label: Text(url.lastPathComponent))
         }
-        .receive(on: )
         .assign(to: \RemoteImage.image, on: self)
         
 
