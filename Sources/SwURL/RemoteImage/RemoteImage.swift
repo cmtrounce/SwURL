cdd62c916f5062012fe347402a4ec1af34c0a802
Callum Trounce <trounce@d-tt.nl>
2019-06-07 12:04:52 +0200

    fixed image not being set correctly.

--- Sources/SwURL/RemoteImage/RemoteImage.swift
+++ Sources/SwURL/RemoteImage/RemoteImage.swift
@@ -20,7 +20,7 @@ class RemoteImage: BindableObject {
     var request: Cancellable?
     
     var image: Image? = nil {
-        didSet {
+        willSet {
             guard image == nil else { return }
             DispatchQueue.main.async {
                 self.didChange.send(self.image!)
