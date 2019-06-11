2d8933aa4813fc058333353efe7291e39839af72
Callum Trounce <trounce@d-tt.nl>
2019-06-11 23:26:50 +0200

    created Networker.swift

--- Sources/SwURL/SwURL.swift
+++ Sources/SwURL/SwURL.swift
@@ -1,5 +1,14 @@
+import Combine
+import Foundation
+
+@available(iOS 13.0, *)
 struct SwURL {
     
+    private static let networker = Networker()
     
+    static func requestDecodable<T: Decodable>(from url: URL,
+                                        decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<T, Error> {
+       return networker.requestDecodable(from: url, decoder: decoder)
+    }
     
 }
