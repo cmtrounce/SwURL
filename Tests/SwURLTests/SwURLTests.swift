a6f6817caab801e8d22a1ea9014773f7dce20f1c
Callum Trounce <trounce@d-tt.nl>
2019-08-03 09:15:04 +0100

    empty the test class.

--- Tests/SwURLTests/SwURLTests.swift
+++ Tests/SwURLTests/SwURLTests.swift
@@ -5,18 +5,4 @@ import XCTest
 final class SwURLTests: XCTestCase {
     
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        
-      
-        let _ = RemoteImage.init().load(url: URL.init(string: "https://lorempixel.com/100/100/cats/")!)
-        XCTAssert(true)
-        
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
