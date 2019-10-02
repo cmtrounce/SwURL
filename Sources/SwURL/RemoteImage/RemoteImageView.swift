dd4458947118d1fbcc1d3096c5203c66f12996d6
Callum Trounce <trounce@d-tt.nl>
2019-10-02 23:16:38 +0100

    changed placeholder percentage.

--- Sources/SwURL/RemoteImage/RemoteImageView.swift
+++ Sources/SwURL/RemoteImage/RemoteImageView.swift
@@ -27,7 +27,7 @@ public struct RemoteImageView: View {
 			finalImage: remoteImage.load(url: url).image?
 				.resizable()
 				.renderingMode(imageRenderingMode),
-			percentageComplete: CGFloat(50),
+			percentageComplete: CGFloat(0.5),
 			transitionType: transitionType
 		)
     }
