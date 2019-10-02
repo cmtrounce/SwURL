73da3d012fad446c560da8ab47f2cb42ae52826f
Callum Trounce <trounce@d-tt.nl>
2019-10-02 22:56:15 +0100

    removed stupid text

--- Sources/SwURL/RemoteImage/ProgressBar.swift
+++ Sources/SwURL/RemoteImage/ProgressBar.swift
@@ -15,31 +15,22 @@ struct ProgressBar: View {
 		let frame = geometry.frame(in: .global)
 		return frame.size.width * progressValue
 	}
-
-	func getPercentage(_ value: CGFloat) -> String {
-		let intValue = Int(ceil(value * 100))
-		return "\(intValue) %"
-	}
 	
 	let progressValue: CGFloat
 	
 	var body: some View {
 		GeometryReader { geometry in
-			VStack(alignment: .trailing) {
-				Text("Progress: \(self.getPercentage(self.progressValue))")
-					.padding()
-				ZStack(alignment: .leading) {
-					Rectangle()
-						.opacity(0.1)
-					Rectangle()
-						.frame(minWidth: 0, idealWidth:self.getProgressBarWidth(geometry: geometry),
-							   maxWidth: self.getProgressBarWidth(geometry: geometry))
-						.opacity(0.5)
-						.background(Color.green)
-						.animation(.default)
-				}
-				.frame(height:10)
-			}.frame(height:10)
+			ZStack(alignment: .center) {
+				Rectangle()
+					.opacity(0.1)
+				Rectangle()
+					.frame(minWidth: 0, idealWidth: self.getProgressBarWidth(geometry: geometry),
+						   maxWidth: self.getProgressBarWidth(geometry: geometry))
+					.opacity(0.5)
+					.background(Color.green)
+					.animation(.default)
+			}
+			.frame(height:10)
 		}
 	}
 }
